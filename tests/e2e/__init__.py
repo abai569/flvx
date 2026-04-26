41e1f1849687588244d110c9655e93a8e4048971
abai569 <abaibubaix@gmail.com>
2026-04-27 00:41:26 +0800

    chore: 删除测试文件目录，修复 meta 标签警告

--- tests/e2e/__init__.py
+++
@@ -1,6 +0,0 @@
-# E2E Tests Package
-"""
-FLVX End-to-End Testing Suite
-"""
-
-__version__ = "0.1.0"
--- tests/e2e/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utils package for E2E tests."""
-
-from .api_client import APIClient, TestUser
-
-__all__ = ["APIClient", "TestUser"]
